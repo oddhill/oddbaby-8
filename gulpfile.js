cddad1c31e497f815c5d921f8d197fe789bbb75f
Jens Lind <jens.lind@oddhill.se>
2016-01-04 08:56:46 +0100

    Make sure gulpfile is following lint rules

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,37 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-var eslint = require('gulp-eslint')
-var scsslint = require('gulp-scss-lint')
-var browserify = require('browserify')
-var shim = require('browserify-shim')
-var babelify = require('babelify')
-var cssGlobbing = require('gulp-css-globbing')
-var source = require('vinyl-source-stream')
-var drupalBreakpoints = require('drupal-breakpoints-scss')
-var rename = require('gulp-rename')
-var uglify = require('gulp-uglify')
-var buffer = require('vinyl-buffer')
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const eslint = require('gulp-eslint');
+const scsslint = require('gulp-scss-lint');
+const browserify = require('browserify');
+const shim = require('browserify-shim');
+const babelify = require('babelify');
+const cssGlobbing = require('gulp-css-globbing');
+const source = require('vinyl-source-stream');
+const drupalBreakpoints = require('drupal-breakpoints-scss');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const buffer = require('vinyl-buffer');
 
 // babel
 gulp.task('browserify', function () {
-  var b = browserify({
+  const b = browserify({
     basedir: './js',
     entries: './main.js'
-  })
+  });
 
-  b.transform(babelify, {presets: ['es2015']})
-  b.transform(shim, {global: true})
+  b.transform(babelify, {presets: ['es2015']});
+  b.transform(shim, { global: true });
 
   return b.bundle()
     .on('error', function (err) {
-      console.log(err.message)
-      this.emit('end')
+      console.log(err.message);
+      this.emit('end');
     })
     .pipe(source('bundle.js'))
     .pipe(buffer())
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/js'))
-})
+    .pipe(gulp.dest('./dist/js'));
+});
 
 // Compile sass
 gulp.task('sass', function () {
@@ -39,7 +39,7 @@ gulp.task('sass', function () {
   gulp.src('./*.breakpoints.yml')
     .pipe(drupalBreakpoints.ymlToScss())
     .pipe(rename('_breakpoints.scss'))
-    .pipe(gulp.dest('./scss/utils'))
+    .pipe(gulp.dest('./scss/utils'));
 
   return gulp.src(['./scss/main.scss', './scss/print.scss'])
     .pipe(cssGlobbing({
@@ -52,27 +52,27 @@ gulp.task('sass', function () {
         'node_modules/Stratagem/'
       ]
     }).on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'))
-})
+    .pipe(gulp.dest('./dist/css'));
+});
 
 // Lint sass
 gulp.task('scss-lint', function () {
   return gulp.src(['./scss/**/*.scss', '!./scss/print.scss', '!scss/normalize.scss', '!scss/vendor/**/*'])
-    .pipe(scsslint())
-})
+    .pipe(scsslint());
+});
 
 // Eslint
 gulp.task('eslint', function () {
   return gulp.src('./js/**/*.js')
     .pipe(eslint())
-    .pipe(eslint.format())
-})
+    .pipe(eslint.format());
+});
 
 // Watch .scss and .js
 gulp.task('watch', function () {
-  gulp.watch('scss/**/*.scss', ['scss-lint', 'sass'])
-  gulp.watch('js/**/*.js', ['eslint', 'browserify'])
-})
+  gulp.watch('scss/**/*.scss', ['scss-lint', 'sass']);
+  gulp.watch('js/**/*.js', ['eslint', 'browserify']);
+});
 
 // Set default task
-gulp.task('default', ['watch'])
+gulp.task('default', ['watch']);
